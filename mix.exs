4ff3888e1191c457c3809d8a0b2a098fcfaf1a43
mburuwarui <mburuwarui@gmail.com>
2024-01-08 16:49:17 +0300

    add bodyguard dep for authorization

--- mix.exs
+++ mix.exs
@@ -51,7 +51,8 @@ defmodule Amboseli.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:bodyguard, "~> 2.4"}
     ]
   end
 
