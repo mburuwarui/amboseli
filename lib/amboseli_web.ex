eba57a168288be21d0ae5753f67dacd30c6cdde7
mburuwarui <mburuwarui@gmail.com>
2024-01-28 20:08:28 +0300

    favicons

--- lib/amboseli_web.ex
+++ lib/amboseli_web.ex
@@ -17,7 +17,9 @@ defmodule AmboseliWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths,
+    do:
+      ~w(assets fonts images favicon.ico apple-touch-icon.png favicon-32x32.png favicon-16x16.png safari-pinned-tab.svg site.webmanifest robots.txt)
 
   def router do
     quote do
